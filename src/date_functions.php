17b3c84ff3e5246418aff4bd11fd8aae0909d738
Sander Bos <sanderbos17@icloud.com>
2021-06-14 16:12:59 +0200

    Reverted some code for demo

--- src/date_functions.php
+++ src/date_functions.php
@@ -9,5 +9,5 @@
  * @throws Exception
  */
 function is_between(string $date, string $from, string $till): bool {
-    return new DateTime($from) <= new DateTime($date) && new DateTime($till) >= new DateTime($date);
+    return new DateTime($from) < new DateTime($date) || new DateTime($till) > new DateTime($date);
 }
